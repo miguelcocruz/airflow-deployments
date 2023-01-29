2063b94d47c20cd3806da2fe1017c184339d4e48
miguelcruz <miguelcocruz@gmail.com>
2023-01-29 18:55:18 +0000

    Fix dep07 - Add egress to ecs service sg

--- dep07/terraform/main.tf
+++ dep07/terraform/main.tf
@@ -205,4 +205,10 @@ resource "aws_security_group" "ecs_service" {
     security_groups = [aws_security_group.lb.id]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
